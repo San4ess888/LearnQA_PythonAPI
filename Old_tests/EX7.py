d2c26bf1cd361980e36bd8ab0b4e4b820627b00b
San4ess888 <aochulikov@gmail.com>
2021-08-14 10:33:11 +0300

    1

--- Old_tests/EX7.py
+++ Old_tests/EX7.py
@@ -1,18 +1,1 @@
-import requests
-parameters = ["POST", "PUT", "GET","DELETE"]
-response1 = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method":"GET"})
-print(response1.text)
-response2 = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method":"HEAD"})
-print(response2.text)
-for i in range(len(parameters)):
-    response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method":parameters[i]})
-    print(response.text)
-for i in range(len(parameters)):
-    response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method":parameters[i]})
-    print(response.text)
-for i in range(len(parameters)):
-    response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method":parameters[i]})
-    print(response.text)
-for i in range(len(parameters)):
-    response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method":parameters[i]})
-    print(response.text)
+
